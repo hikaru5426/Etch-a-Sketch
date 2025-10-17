98217521a4881b458907168901a383e73e1f1eee
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-17 13:47:27 +0200

    Cells change color when clicked on and passed on with mouse button down

--- javascript.js
+++ javascript.js
@@ -1,19 +1,47 @@
-const container = document.querySelector("#container");
+document.addEventListener("mousedown", (e) => {
+    if (e.button === 0) {
+        mouseDown = true;
+    }
+});
+
+document.addEventListener("mouseup", (e) => {
+    if (e.button === 0) {
+        mouseDown = false;
+    }
+});
 
 
-
-function createTable(){
-    for(let column = 0; column <= 15; column ++){
-        for(let line = 0; line <= 15; line ++){
+function createTable() {
+    for (let column = 0; column <= 15; column++) {
+        for (let line = 0; line <= 15; line++) {
             createDiv(column, line);
         }
     }
 }
 
-function createDiv(column, line){
+function createDiv(column, line) {
     let div = document.createElement("div");
-        div.textContent = String(16*column+line);
-        container.appendChild(div);
+    div.textContent = String(16 * column + line);
+
+    // To change color only once when user click
+    div.setAttribute("colorChanged", false);
+    div.addEventListener("mouseenter", setTrail);
+    div.addEventListener("mousedown", setTrail);
+    container.appendChild(div);
 }
 
+function setTrail(event) {
+    let div = event.target;
+    if (event.type === "mouseenter") {
+        if (mouseDown) {
+            div.style.backgroundColor = "pink";
+        }
+    }else if (event.type === "mousedown"){
+        div.style.backgroundColor = "green";
+    }
+}
+
+
+const container = document.querySelector("#container");
+let mouseDown = false;
 createTable();
